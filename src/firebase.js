9773b565572f9e4c129f926c4aa7ec8081b9f314
Giacomo <gtarraran992@gmail.com>
2026-04-29 17:27:30 +0200

    part-propic

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 import { getFirestore, initializeFirestore, persistentLocalCache, persistentMultipleTabManager } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -13,6 +14,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig)
 
+export const storage = getStorage(app)
 export const auth = getAuth(app)
 export const googleProvider = new GoogleAuthProvider()
 export const db = initializeFirestore(app, {
